8534cce3f493aad038f6f38f19a0c921770a33d7
Róbert Viðar Bjarnason <robert@citizens.is>
2014-02-26 19:09:11 +0000

    Debugging IE8 on staging

--- vendor/assets/javascripts/selectivizr-1.0.3b.js
+++ vendor/assets/javascripts/selectivizr-1.0.3b.js
@@ -30,8 +30,7 @@ References:
 	// being loaded by non IE browsers because the developer didn't use
 	// conditional comments.
         alert("selectwiz1");
-	//if (/*@cc_on!@*/true) return;
-        alert("selectwiz2");
+	if (/*@cc_on!@*/true) return;
 
 	// =========================== Init Objects ============================
 
@@ -42,10 +41,9 @@ References:
 	alert(navigator.userAgent);
 	// If were not in standards mode, IE is too old / new or we can't create
 	// an XMLHttpRequest object then we should get out now.
-	//if (doc.compatMode != 'CSS1Compat' || ieVersion<6 || ieVersion>8 || !xhr) {
-	//	return;
-	//}
-    alert("selectwiz3");
+	if (doc.compatMode != 'CSS1Compat' || ieVersion<6 || ieVersion>8 || !xhr) {
+		return;
+	}
     alert(ieVersion);
 	// ========================= Common Objects ============================
 
