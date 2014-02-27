ffa9e0700a92233b9b7608040d8785d9c63f0102
Róbert Viðar Bjarnason <robert@citizens.is>
2014-02-27 11:21:03 +0000

    Debugging IE8 on staging

--- public/selectivizr-1.0.3b.js
+++ public/selectivizr-1.0.3b.js
@@ -29,7 +29,7 @@ References:
 	// If browser isn't IE, then stop execution! This handles the script
 	// being loaded by non IE browsers because the developer didn't use
 	// conditional comments.
-        alert("selectwiz1");
+    //alert("selectwiz1");
 	if (/*@cc_on!@*/true) return;
 
 	// =========================== Init Objects ============================
