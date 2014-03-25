a309c279b8c7e24ce8189bd1f9f05826a7325ae0
Róbert Viðar Bjarnason <robert@citizens.is>
2014-03-25 10:03:42 +0000

    Removed unused

--- config/initializers/app_customization.rb
+++ config/initializers/app_customization.rb
@@ -8,4 +8,3 @@ HOW_LONG_SHALL_NEW_POINTS_LIVE_IN_TOP_POINTS = 15.minutes
 
 NB_CONFIG = { 'api_exclude_fields' => [:ip_address, :user_agent, :referrer, :google_token, :google_crawled_at, :activation_code, :salt, :email, :first_name, :last_name, :crypted_password, :is_tagger, :sub_instance_id, :ip_address, :user_agent, :remember_token, :remember_token_expires_at, :referrer, :zip, :birth_date, :city, :state, :is_comments_subscribed, :is_finished_subscribed, :is_followers_subscribed, :is_mergeable, :is_capital_subscribed, :is_messages_subscribed, :report_frequency, :is_point_changes_subscribed, :is_subscribed, :is_idea_changes_subscribed, :contacts_count, :contacts_invited_count, :contacts_members_count, :contacts_not_invited_count, :code, :rss_code, :address] }
 
-seed_notification = Notification.new
